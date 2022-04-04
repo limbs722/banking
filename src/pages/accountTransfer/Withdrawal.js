8347e4a23d89454a9fca063b9718ae310946cbf0
limbs722 <limbs722@gmail.com>
2022-04-04 10:49:09 +0900

    Remove - 사용하지 않는 파일 삭제

--- src/pages/accountTransfer/Withdrawal.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Withdrawal = () => {};
-
-export default Withdrawal;
--- src/pages/myAccount/AccountCreate.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function AccountCreate() {
-  return <div></div>;
-}
-
-export default AccountCreate;
-// my account
-// account Transfer
